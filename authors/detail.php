63c12a497d2dcf873bb4ca09d69a63df0f96934d
Tedla2001 <tedlamariye99@outlook.com>
2022-10-16 21:54:15 -0400

    implement modify for author name

--- authors/detail.php
+++ authors/detail.php
@@ -44,9 +44,14 @@ function printQuote($filename,$index){
   </div>
 
 <h1 class="top-header"><?= $author_name ?></h1>
-
+<div class="add-button-div">
+  <a  href="modify.php?index=<?=$index?>"><button type="button" class="btn btn-outline-warning">Modify Author</button></a>
+  <a  href="delete.php?index=<?=$index?>"><button type="button" class="btn btn-outline-danger">Delete Author</button></a>
+  </div>
 <div class="center-screen">
 <?= printQuote('../data/quotes.csv', $index) ?>
+
+
 </div>
 
 <script script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
